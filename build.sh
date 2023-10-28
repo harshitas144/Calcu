f29423984e373efc4ab110b9d58e094c743ac944
harshitagitactions <harshitasaug@gmail.com>
2023-10-28 21:08:00 +0530

    Add build script and update CI/CD workflow

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script runs a Python file that performs addition and subtraction.
+
+# Run your Python script (replace 'calculator.py' with your file's name)
+python calculator.ipynb
