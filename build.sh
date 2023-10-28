e66ee6a77ebfd553ce55793f7467957e3b34f3ea
harshitagitactions <harshitasaug@gmail.com>
2023-10-28 23:51:37 +0530

    Update build.sh for testing

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-# This script runs a Python file that performs addition and subtraction.
+# Build your application (if applicable)
+# For a simple calculator program, you may not have a build step.
 
-# Run your Python script (replace 'calculator.py' with your file's name)
-python calculator.ipynb
-# Run tests using pytest (replace with your testing framework)
-pytest caltest_py.py
+# Activate a virtual environment (if applicable)
+# Replace 'venv' with the name of your virtual environment if you're using one.
+source venv/bin/activate
+pip install pytest
+
+# Run your tests using pytest (replace 'test_calculator.py' with your test file)
+pytest test_calculator.py
+
+# If you don't have tests, you can run your calculator program (replace 'calculator.py' with your program)
+python calculator.py
+
+# Deactivate the virtual environment (if applicable)
+deactivate
