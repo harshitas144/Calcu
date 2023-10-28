efb56d4a5f79c93c327113282091157c33dbf325
harshitagitactions <harshitasaug@gmail.com>
2023-10-28 23:39:46 +0530

    Update build.sh for testing

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@
 
 # Run your Python script (replace 'calculator.py' with your file's name)
 python calculator.ipynb
+# Run tests using pytest (replace with your testing framework)
 pytest caltest_py.py
